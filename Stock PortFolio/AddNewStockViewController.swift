34835e11dc6fa68bd404cdfdac020f191ad21ac1
Matthew <matt@Matthews-MacBook-Pro.local>
2022-04-12 09:32:06 -0600

    AddNewStockView
    Added a segment control so you can pick between basic and advanced. Got the code working for that.

--- Stock PortFolio/AddNewStockViewController.swift
+++ Stock PortFolio/AddNewStockViewController.swift
@@ -13,23 +13,65 @@ class AddNewStockViewController: UIViewController {
     @IBOutlet var totalShares: UITextField!
     
     @IBOutlet var timeBoughtDatePicker: UIDatePicker!
+    @IBOutlet var timeBoughtLabel: UILabel!
+    @IBOutlet var timeBoughtLabelThatIsntDate: UILabel!
+    
     @IBOutlet var timeSoldDatePicker: UIDatePicker!
+    @IBOutlet var timeSoldLabel: UILabel!
+    @IBOutlet var timeSoldLabelThatIsntDate: UILabel!
     
     @IBOutlet var tickerSymbol: UITextField!
     
+    @IBOutlet var basicOrAdvanced: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        timeSoldLabel.isHidden = true
+        timeSoldLabelThatIsntDate.isHidden = true
+        timeBoughtLabel.isHidden = true
+        timeBoughtLabelThatIsntDate.isHidden = true
+        
+        
+        let tapTimeBoughtLabel = UITapGestureRecognizer(target: self, action: #selector(AddNewStockViewController.tapBoughtFunction))
+        timeBoughtLabel.isUserInteractionEnabled = true
+        timeBoughtLabel.addGestureRecognizer(tapTimeBoughtLabel)
+        
+        let tapTimeSoldLabel = UITapGestureRecognizer(target: self, action: #selector(AddNewStockViewController.tapSoldFunction))
+        timeSoldLabel.isUserInteractionEnabled = true
+        timeSoldLabel.addGestureRecognizer(tapTimeSoldLabel)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func indexChanged(_ sender: UISegmentedControl) {
+        switch basicOrAdvanced.selectedSegmentIndex {
+        case 0:
+            timeSoldLabel.isHidden = true
+            timeSoldLabelThatIsntDate.isHidden = true
+            timeBoughtLabel.isHidden = true
+            timeBoughtLabelThatIsntDate.isHidden = true
+        case 1:
+            timeSoldLabel.isHidden = false
+            timeSoldLabelThatIsntDate.isHidden = false
+            timeBoughtLabel.isHidden = false
+            timeBoughtLabelThatIsntDate.isHidden = false
+        default:
+            break
+        }
     }
-    */
-
+    
+    @objc func tapBoughtFunction(sender: UITapGestureRecognizer) {
+        timeSoldLabelThatIsntDate.isHidden = true
+        timeSoldLabel.isHidden = true
+        timeBoughtDatePicker.isHidden = false
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+            self.timeSoldLabel.isHidden = false
+            self.timeSoldLabelThatIsntDate.isHidden = false
+            self.timeBoughtDatePicker.isHidden = true
+            self.timeSoldDatePicker.isHidden = false
+        }
+    }
+    
+    @objc func tapSoldFunction(sender: UITapGestureRecognizer) {
+        timeSoldDatePicker.isHidden = false
+    }
 }
